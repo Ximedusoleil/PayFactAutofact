136e98c50bc799b2116ea5242dceed42d97b99e7
Ximedusoleil <ximena.jara.lobo@gmail.com>
2021-08-23 16:53:23 -0400

    Version 1

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+  $("#Ingresar").click(
+      function(){
+          alert("Ingresando a tu cuenta.");
+      }
+  )
+});
+
+$(function(){
+  $("#Crear_cuenta").click(
+      function(){
+          alert("Cuenta creada. Accede a tu cuenta.");
+      }
+  )
+});
+
+$(function(){
+  $("#Enviar_mensaje").click(
+      function(){
+          alert("Mensaje enviado. Te contactaremos.");
+      }
+  )
+});
